4f15fd8ac87fb08d20fdcc1cd3529cbd15e1ec0d
Ali Home <Alimohammadzade7976@gmail.com>
2023-03-28 20:25:14 +0430

    1402/01/08 08:24 design Infrastructure layer adding mappings adding dbContext adding dbsets Implementig repository

---
+++ ShopManagement.Infrastructure.EfCore/Repository/ProductCategoryRepository.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Linq.Expressions;
+using ShopManagement.Application.Contracts.ProductCategoryContracts;
+using SM.Domain.ProductCategoryAgg;
+
+namespace ShopManagement.Infrastructure.EfCore.Repository
+{
+    public class ProductCategoryRepository:IProductCategoryRepository
+    {
+        private readonly ShopContext _context;
+
+        public ProductCategoryRepository(ShopContext context)
+        {
+            _context = context;
+        }
+
+        public void Create(ProductCategory entity)
+        {
+            _context.ProductCategories.Add(entity);
+        }
+
+        public ProductCategory Get(long id)
+        {
+            return _context.ProductCategories.Find(id);
+        }
+
+        public List<ProductCategory> GetAll()
+        {
+          return  _context.ProductCategories.ToList();
+        }
+
+        public bool IsExists(Expression<Func<ProductCategory, bool>> predicate)
+        {
+          return  _context.ProductCategories.Any(predicate);
+        }
+
+        public void SaveChanges()
+        {
+            _context.SaveChanges();
+        }
+
+        public List<ProductCategoryViewModel> Search(SearchProductCategoryModel searchModel)
+        {
+            var query = _context.ProductCategories.Select(x => new ProductCategoryViewModel
+            {
+                Id = x.Id,
+                CreationDate = x.CreationDate.ToString(CultureInfo.InvariantCulture),
+                Name = x.Name,
+                Picture = x.Picture
+            });
+            query = query.Where(x => x.Name.Contains(searchModel.Name)).OrderByDescending(x=>x.Id);
+            return query.ToList();
+        }
+
+    }
+}
---
+++ ShopManagement.Infrastructure.EfCore/ShopContext.cs
@@ -0,0 +1,24 @@
+﻿using System.Reflection;
+using Microsoft.EntityFrameworkCore;
+using ShopManagement.Infrastructure.EfCore.Mapping;
+using SM.Domain.ProductCategoryAgg;
+
+namespace ShopManagement.Infrastructure.EfCore
+{
+    public class ShopContext:DbContext
+    {
+        public DbSet<ProductCategory> ProductCategories { get; set; }
+
+        public ShopContext(DbContextOptions<ShopContext> options):base(options)
+        {
+            
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            Assembly assembly = typeof(ProductCategoryMapping).Assembly;
+            modelBuilder.ApplyConfigurationsFromAssembly(assembly);
+            base.OnModelCreating(modelBuilder);
+        }
+    }
+}
