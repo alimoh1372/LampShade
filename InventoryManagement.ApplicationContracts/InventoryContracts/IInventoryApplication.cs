98e9af096920e38d5b189d7d71a8e87ed1e0a681
Ali From NocEmam <alimohammadzade7976@gmail.com>
2023-04-08 17:42:57 +0330

    1402/01/19 05:42
    Inventory Domain and application contract layer implementing

---
+++ InventoryManagement.ApplicationContracts/InventoryContracts/IInventoryApplication.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections.Generic;
+using _0_Framework.Application;
+
+namespace InventoryManagement.ApplicationContracts.InventoryContracts
+{
+    public interface IInventoryApplication
+    {
+        OperationResult CreateInventory(CreateInventory command);
+        OperationResult EditInventory(EditInventory command);
+        OperationResult IncreaseInventory(IncreaseInventory command);
+        OperationResult ReduceInventories(List<ReduceInventory> command);
+        OperationResult ReduceInventory(ReduceInventory command);
+        EditInventory GetDetails(long id);
+        List<InventoryViewModel> Search(InventorySearchModel searchModel);
+
+    }
+
+    public class InventorySearchModel   
+    {
+    }
+
+    public class InventoryViewModel
+
+    {
+    }
+
+    public class ReduceInventory
+    {
+    }
+
+    public class IncreaseInventory
+    {
+    }
+
+    public class EditInventory
+
+    {
+    }
+
+    public class CreateInventory
+    {
+    }
+}
---
+++ InventoryManagement.Domain/InventoryAgg/Inventory.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using _0_Framework.Domain;
+
+namespace InventoryManagement.Domain.InventoryAgg
+{
+    public class Inventory:EntityBase
+    {
+        public long FkProductId { get; private set; }
+        public double UnitPrice { get; private set; }
+        public bool IsInStock { get; private set; }
+        public List<InventoryOperation> InventoryOperations { get; private set; }
+
+        public Inventory(long fkProductId, double unitPrice)
+        {
+            FkProductId = fkProductId;
+            UnitPrice = unitPrice;
+            IsInStock = false;
+        }
+
+        private long CalculateCurrentCount()
+        {
+            var plus = InventoryOperations.Where(x => x.Operation).Sum(x => x.Count);
+            var minus = InventoryOperations.Where(x => !x.Operation).Sum(x => x.Count);
+            return plus - minus;
+        }
+
+        public void IncreaseInventory(long count, long fkOperatorId, string description)
+        {
+            long currentCount = CalculateCurrentCount() + count;
+            InventoryOperation inventoryOperation =
+                new InventoryOperation(true, count, fkOperatorId, currentCount, description, 0, Id);
+            
+            InventoryOperations.Add(inventoryOperation);
+            IsInStock = CalculateCurrentCount() > 0;
+        }
+
+        public void Reduce(long count, long fkOperatorId, long fkOrderId, string description)
+        {
+            long currantCount = CalculateCurrentCount() - count;
+            InventoryOperation inventoryOperation =
+                new InventoryOperation(false, count, fkOperatorId, currantCount, description, fkOrderId, Id);
+            InventoryOperations.Add(inventoryOperation);
+            IsInStock = CalculateCurrentCount() > 0;
+        }
+    }
+
+    public class InventoryOperation
+    {
+        public long Id { get; private set; }
+
+        public bool Operation { get; private set; }
+        public long Count { get; private set; }
+        public long FkOperatorId { get; private set; }
+        public DateTime OperationDate { get; private set; }
+        public long CurrentCount { get; private set; }
+        public string Description { get; private set; }
+        public long OrderId { get; private set; }
+        public long FkInventoryId { get; private set; }
+
+        public Inventory Inventory { get; private set; }
+
+        public InventoryOperation(bool operation, long count, long fkOperatorId, long currentCount,
+            string description, long orderId, long fkInventoryId)
+        {
+            Operation = operation;
+            Count = count;
+            FkOperatorId = fkOperatorId;
+            CurrentCount = currentCount;
+            Description = description;
+            OrderId = orderId;
+            FkInventoryId = fkInventoryId;
+            OperationDate=DateTime.Now;
+
+        }
+    }
+}
